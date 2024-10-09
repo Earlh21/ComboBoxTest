3a06a4e462eaf76918800f8331dcd42947d13232
Earlh21 <idrialite@gmail.com>
2024-10-09 12:45:13 -0400

    .

--- ComboBoxTest/ViewModels/MainWindowViewModel.cs
+++ ComboBoxTest/ViewModels/MainWindowViewModel.cs
@@ -2,11 +2,14 @@
 using System.Collections.ObjectModel;
 using ComboBoxTest.Models;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace ComboBoxTest.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
+    [ObservableProperty] private TestModel modelProperty;
+    
     public ObservableCollection<TestEnum> EnumValues { get; } = [];
     public ObservableCollection<TestModel> Models { get; } = [];
     
@@ -16,7 +19,14 @@ public partial class MainWindowViewModel : ViewModelBase
         {
             EnumValues.Add(value);
         }
+        
+        ModelProperty = new() { EnumProperty = TestEnum.EnumValue1 };
+        Models.Add(new() { EnumProperty = TestEnum.EnumValue1 });
+    }
 
+    [RelayCommand]
+    private void AddModel()
+    {
         Models.Add(new() { EnumProperty = TestEnum.EnumValue1 });
     }
 }
